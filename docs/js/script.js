6e899ab8a3b58ab1d8e06652358188c2546ff2a1
goolyb <po7795341@gmail.com>
2026-06-07 17:07:26 +0200

    added some features

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,5 @@
-
+window.addEventListener('scroll', () => {
+    const scrollOverlay = document.querySelector('.scroll-overlay');
+    const scrollPercent = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
+    scrollOverlay.style.opacity = scrollPercent * 0.5;
+});
